10019490d2016ef9d083d6ad1a5921687617ec5a
mariam <mariam@MYPROBOOK>
2023-03-03 19:46:58 +0400

    Add project files.

---
+++ Restaurant03.03/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Restaurant03._03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Restaurant rest4581;//uxaki haytararutyuna like int t;
+
+            //rest4581.t = 85;
+            Restaurant rest = new Restaurant();//sarquma instance default ctoric ogtvelov
+            Console.WriteLine(rest.t);   //deafult 0
+            Console.WriteLine(rest.MakeFood(Menu.Pizza, 6));
+
+
+            Restaurant rest2 = new Restaurant(66);//sarquma instance poxancelov ctor in 66 tivn
+            Console.WriteLine(rest2.t);//66
+            Console.WriteLine(rest2.MakeFood(Menu.HotDog, 5));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Restaurant03.03/Restaurant.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant03._03
+{
+    internal class Restaurant
+    {
+        public Restaurant()
+        {
+
+        }
+        public Restaurant(int t)
+        {
+            this.t = t;
+            PrintAnkapBan();
+        }
+        public int t;
+        public const string Address = "Abovyan-1";
+        public const string Number = "+37496666666";
+
+        public void PrintAnkapBan()
+        {
+            Console.WriteLine("printAnkapBan");
+        }
+        public decimal MakeFood(Menu menu, int count)
+
+        {
+            switch (menu)
+            {
+                case Menu.HotDog:
+                    return (decimal)Menu.HotDog * (count + t);
+                case Menu.Sushi:
+                    return (decimal)Menu.Sushi * (count + t);
+                case Menu.Pizza:
+                    return (decimal)Menu.Pizza * (count + t);
+                case Menu.Shawrma:
+                    return (decimal)Menu.Shawrma * (count + t);
+                case Menu.Xash:
+                    return (decimal)Menu.Xash * (count + t);
+                default:
+                    return 0;
+            }
+        }
+
+    }
+    public enum Menu
+    {
+        HotDog = 500,
+        Sushi = 12000,
+        Pizza = 300,
+        Shawrma = 750,
+        Xash = 1350
+    }
+}
+
+
